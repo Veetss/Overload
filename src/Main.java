f9f652a2f1fbe7a098d31e7f9ac377511aa66910
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:45:50 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Shape circle = new Shape(5.5);
+        Shape square = new Shape(4, 5.7);
+        Shape rectangle = new Shape(4, 5.8, 10.9);
+        Shape triangle = new Shape(3, 5.6, 5.9, 6.6);
+        Shape undefined = new Shape();
+
+        String circleVar = circle.getShapeDetails();
+        String squareVar = square.getShapeDetails();
+        String rectangleVar = rectangle.getShapeDetails();
+        String triangleVar = triangle.getShapeDetails();
+        String undefinedVar = undefined.getShapeDetails();
+
+        System.out.println(circleVar);
+        System.out.println(squareVar);
+        System.out.println(rectangleVar);
+        System.out.println(triangleVar);
+        System.out.println(undefinedVar);
     }
 }
