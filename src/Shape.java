8c43fe89f6aee1b8e59e12e2407ae77a7086721a
Vittoria Vassarotti <112390959+Veetss@users.noreply.github.com>
2022-12-07 11:46:11 +0100

    Update Shape.java

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Shape {
+public class Shape {
+    public String shapeName;
+    public int numberOfEdges;
+
+    public Shape(){
+        System.out.println("Creating of a shape ");
+        this.shapeName = null;
+    }
+
+    public Shape(double radius){
+        this.shapeName = "Circle";
+        System.out.println(shapeName + " creation in progress...");
+    }
+    public Shape(int edges, double edgeLenght){
+        this.shapeName = "Square";
+        this.numberOfEdges = edges;
+        System.out.println(shapeName + " creation in progress...");
+    }
+    public Shape(int edges, double e1, double e2){
+        this.shapeName = "Rectangle";
+        this.numberOfEdges = edges;
+        System.out.println(shapeName + " creation in progress...");
+    }
+    public Shape(int edges, double e1, double e2, double e3){
+        this.shapeName = "Triangle";
+        this.numberOfEdges = edges;
+        System.out.println(shapeName + " creation in progress...");
+    }
+
+    public String getShapeDetails(){
+        return "The " + shapeName + " has " + numberOfEdges + " number of edges!";
+    }
 }
